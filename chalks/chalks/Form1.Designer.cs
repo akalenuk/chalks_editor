d2b20418d485ea1205b52868561fd36ad5206c90
Alexandr Kalenuk <akalenuk@gmail.com>
2014-08-17 06:25:42 +0300

    Initial

---
+++ chalks/chalks/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApplication1 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Silver;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(492, 336);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(492, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 360);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ chalks/chalks/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 {
+    public partial class Form1 : Form {
+
+        public struct Win32Point
+        {
+            public int x;
+            public int y;
+        }
+        public const int WM_USER = 0x400;
+        public const int EM_GETSCROLLPOS = (WM_USER + 221);
+        public const int EM_SETSCROLLPOS = (WM_USER + 222);
+
+        [DllImport("user32")]
+        public static extern int LockWindowUpdate(System.IntPtr hwnd);
+        [DllImport("user32")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, ref Win32Point pt);
+
+        string file_name = "";
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private bool ok_for_name(char c){
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || c=='_') {
+                return true;
+            }
+            return false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) {
+            bool name = false;
+            int name_start = 0;
+            int sel_start = richTextBox1.SelectionStart;
+            int sel_len = richTextBox1.SelectionLength;
+            Win32Point scroll_pos = new Win32Point();
+            SendMessage(richTextBox1.Handle, EM_GETSCROLLPOS, 0, ref scroll_pos);
+
+            LockWindowUpdate(richTextBox1.Handle);
+
+            for (int i = 0; i < richTextBox1.Text.Length; i++) { 
+                if ( ok_for_name( richTextBox1.Text[i] ) ){
+                    if(!name){
+                        name_start = i;
+                        name = true;
+                    }
+                }else{
+                    if(name){
+                        int name_len = i - name_start;
+                        string the_name = richTextBox1.Text.Substring(name_start, name_len);
+                        Random rnd = new Random(the_name.GetHashCode());
+                        
+                        int name_R = rnd.Next(100, 255);
+                        int name_G = rnd.Next(100, 255);
+                        int name_B = rnd.Next(100, 255);
+
+                        richTextBox1.Select(name_start, name_len);
+                        richTextBox1.SelectionColor = Color.FromArgb(name_R, name_G, name_B);
+                        
+                        name = false;
+                    }
+                }
+            }
+            
+            richTextBox1.Select(sel_start, sel_len);
+            SendMessage(richTextBox1.Handle, EM_SETSCROLLPOS, 0, ref scroll_pos);
+            LockWindowUpdate((IntPtr)0);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            openFileDialog1.FileName = file_name;
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK ){                
+                file_name = openFileDialog1.FileName;
+                richTextBox1.LoadFile(file_name, RichTextBoxStreamType.PlainText);
+                saveFileDialog1.FileName = file_name;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            if(saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK ){
+                file_name = saveFileDialog1.FileName;
+                richTextBox1.SaveFile(file_name, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            file_name = "";
+            Random rnd = new Random( DateTime.Now.TimeOfDay.GetHashCode() );
+            int name_R = rnd.Next(0, 50);
+            int name_G = rnd.Next(0, 50);
+            int name_B = rnd.Next(0, 50);
+            richTextBox1.BackColor = Color.FromArgb( name_R, name_G, name_B );
+        }
+    }
+}
